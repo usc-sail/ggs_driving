8ce08602e3d3bfa99388ab44de78e1bee3233365
klean2050 <k.avramidis@windowslive.com>
2022-10-07 11:43:49 -0700

    formalize

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Tracking Stress-level Changes on Drivers
 
 Applying GGS to physio- and bio-signals.
+
+## Datasets
+
+To run experiments you need to download either of the following datasets:
+
+* [AffectiveROAD](https://www.media.mit.edu/tools/affectiveroad/)
+* [DriveDB](https://physionet.org/content/drivedb/1.0.0/)
+* [HCIDriving](https://www.hcilab.org/research/hcilab-driving-dataset/)
+
+After extracting them you should modify the paths in `datasets/__init__.py` accordingly.
+
+## Experiments
+
+You can modify parameters of the experiment at the `main.py` file.
+```
+pip install -r requirements.txt
+python main.py
+```
--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,17 +2,16 @@ from .load_affectiveroad import AffectiveROAD
 from .load_drivedb import DriveDB
 from .load_hcidriving import HCIDriving
 
-def load_dataset(
-    dataset, missing, sample_rate, gt_type, streams
-):
+
+def load_dataset(dataset, missing, sample_rate, gt_type, streams):
     if dataset == "AffectiveROAD":
-        path = f"/home/kavra/Datasets/{dataset}/Database/"
+        path = f"/home/kavra/Datasets/AffectiveROAD/Database/"
         return AffectiveROAD(path, missing, sample_rate, gt_type, streams)
     elif dataset == "DriveDB":
-        path = f"/home/kavra/Datasets/{dataset}/"
+        path = f"/home/kavra/Datasets/physionet.org/files/drivedb/1.0.0/"
         return DriveDB(path, missing, sample_rate, gt_type, streams)
     elif dataset == "HCIDriving":
-        path = f"/home/kavra/Datasets/{dataset}/"
+        path = f"/home/kavra/Datasets/hcilab_driving_dataset/"
         return HCIDriving(path, missing, sample_rate, gt_type, streams)
     else:
         return NotImplementedError
